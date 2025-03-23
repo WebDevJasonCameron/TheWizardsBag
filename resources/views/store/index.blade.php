c52f138aa9ec7f5a1dba527d307100329018d6bd
Jason Cameron <dev.jasoncameron@gmail.com>
2025-03-23 08:36:50 -0500

    building out the scaffold

---
+++ resources/views/store/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shop</title>
+</head>
+<body>
+<h1>Available Items</h1>
+<ul>
+    <li>Magic Duck</li>
+    <li>Magic Knife</li>
+    <li>Magic Dog Whistle</li>
+    <li>Magic Crocks</li>
+    <li>Magic Spoon</li>
+</ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/test', function (Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'fullUrl' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'userAgent' => $request->userAgent(),
-        'header' => $request->header(),
-    ];
-});
-
-Route::get('/users', function (Request $request) {
-    return $request->all();
+Route::get('/store', function () {
+    return view('store.index');
 });
