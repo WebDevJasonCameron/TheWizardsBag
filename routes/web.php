43434ede8d92d00a0469794f5cbb408af6759881
Jason Cameron <dev.jasoncameron@gmail.com>
2025-03-22 19:08:47 -0500

    playing around, not moving forward yet

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function() {
-    return 'The bag';
+Route::get('/test', function (Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'path' => $request->path(),
+        'fullUrl' => $request->fullUrl(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header(),
+    ];
 });
 
-Route::get('/about', function() {
-    return [
-        'name'=>'John Doe',
-        'email'=>'johndoe@example.com',
-    ];
+Route::get('/users', function (Request $request) {
+    return $request->all();
 });
