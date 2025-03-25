d922b1cd01ad7e4dda4c28447e681c630cce8149
Jason Cameron <dev.jasoncameron@gmail.com>
2025-03-25 05:18:42 -0500

    starting with design

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,5 +10,6 @@
 </head>
 <body>
     <h1 class="text-3xl font-bold underline">Welcome to the Wizard's Bag</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda at debitis deserunt dolorum eius esse, in incidunt magni minus nam qui quos suscipit temporibus vero. Et omnis vel velit.</p>
 </body>
 </html>
